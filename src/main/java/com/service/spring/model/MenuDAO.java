8f403ef350bfb31336dc54204315723a2e984f32
semi9801 <kellee9901@gmail.com>
2023-11-17 17:23:49 +0900

    feat:admin-menu

---
+++ src/main/java/com/service/spring/model/MenuDAO.java
@@ -0,0 +1,14 @@
+package com.service.spring.model;
+
+import java.util.List;
+
+import com.service.spring.domain.Menu;
+
+public interface MenuDAO {
+	int addMenu (Menu menu);
+	int updateMenu (Menu menu);
+	int deleteMenu (List<Menu> list);
+	Menu selectMenu (Menu menu);
+	List<Menu> selectMenu ();
+	List<Menu> selectMenuByCategory(String category);
+}
---
+++ src/main/java/com/service/spring/model/impl/MenuDAOImpl.java
@@ -0,0 +1,50 @@
+package com.service.spring.model.impl;
+
+import com.service.spring.domain.Menu;
+import com.service.spring.model.MenuDAO;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MenuDAOImpl implements MenuDAO {
+    // mapper
+    public static final String NS = "mapper.";
+
+    @Autowired
+    private SqlSession sqlSession;
+
+	@Override
+	public int addMenu(Menu menu) {
+		return sqlSession.insert(NS+"addMenu",menu);
+	}
+
+	@Override
+	public int updateMenu(Menu menu) {
+		return sqlSession.update(NS+"updateMenu", menu);
+	}
+
+	@Override
+	public int deleteMenu(List<Menu> list) {
+		return sqlSession.delete(NS+"deleteMenu", list);
+	}
+
+	@Override
+	public Menu selectMenu(Menu menu) {
+		return sqlSession.selectOne(NS+"selectMenu",menu);
+	}
+
+	@Override
+	public List<Menu> selectMenu() {
+		return sqlSession.selectList(NS+"selectMenu",null);
+	}
+
+	@Override
+	public List<Menu> selectMenuByCategory(String category) {
+		return sqlSession.selectList(NS+"selectMenuByCategory", category);
+	}
+
+}
