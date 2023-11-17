6dc4409651b80cbbe5a6822afc3d1acf3f87a9e6
yj95228 <kei02119@naver.com>
2023-11-17 17:44:22 +0900

    Feat: 결제 페이지 구현 및 별점 페이지 윤곽 만들기

--- src/main/java/com/service/spring/model/OrderListDAO.java
+++ src/main/java/com/service/spring/model/OrderListDAO.java
@@ -1,4 +1,10 @@
 package com.service.spring.model;
 
+import com.service.spring.domain.OrderList;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
+
 public interface OrderListDAO {
+    List<OrderList> selectOrderByTable(OrderList orderList) throws Exception;
 }
